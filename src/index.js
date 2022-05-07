356cf04a31024b05ad3a8c5c8ef5a8e55b87824c
Arvind Dhakar <arvindradhakar625@gmail.com>
2022-05-07 12:00:23 +0530

    Added watch mode command related to webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { add, multiply } from './math';
+
+const result = add(5, 6, 7);
+console.log('result -> ', result);
+console.log('add -> ', add(1, 2, 3))
+console.log('multiply -> ', multiply(4, 5));
---
+++ src/math.js
@@ -0,0 +1,2 @@
+export const add = (first, second, third) => first + second + third;
+export const multiply = (first, second) => first * second;
